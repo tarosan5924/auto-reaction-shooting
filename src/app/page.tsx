b48522eec305a03b3acd888ab07adfe17b78f352
tarosan5924 <tarosan5924@gmail.com>
2023-07-24 19:05:40 +0900

    APIトークンの入力を受けられるように変更、stream開始終了のボタン追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+import { useState } from "react";
+
 const Home = (): JSX.Element => {
-  return <div>お試し</div>;
+  const [isStreaming, setIsStreaming] = useState(false);
+  const [apiToken, setApiToken] = useState<string>("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={apiToken}
+        placeholder={"APIトークンを入力"}
+        onChange={(e) => {
+          setApiToken(e.currentTarget.value);
+        }}
+      />
+      <button
+        disabled={isStreaming}
+        onClick={() => {
+          setIsStreaming(true);
+        }}
+      >
+        開始
+      </button>
+      <button
+        disabled={!isStreaming}
+        onClick={() => {
+          setIsStreaming(false);
+        }}
+      >
+        終了
+      </button>
+    </div>
+  );
 };
 
 export default Home;
