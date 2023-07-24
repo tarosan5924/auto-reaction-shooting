103809c8f316a2dff39d1240d629e89194275452
たろ <139237456+tarosan5924@users.noreply.github.com>
2023-07-24 20:04:18 +0900

    LTLのストリーミング実装 (#2)
    * streamするためにmisskey-js追加
    
    * APIトークンの入力を受けられるように変更、stream開始終了のボタン追加
    
    * チャンネルに接続してタイムラインのノートを取得できるように修正
    
    * 切断できるように修正
    
    * 不要な関数定義削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,58 @@
+"use client";
+import { useState } from "react";
+import * as Misskey from "misskey-js";
+
 const Home = (): JSX.Element => {
-  return <div>お試し</div>;
+  const [isStreaming, setIsStreaming] = useState(false);
+  const [apiToken, setApiToken] = useState<string>("");
+  const [channel, setChannel] = useState<null | Misskey.ChannelConnection<{
+    params: null;
+    events: {
+      note: (payload: Misskey.entities.Note) => void;
+    };
+    receives: null;
+  }>>(null);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={apiToken}
+        placeholder={"APIトークンを入力"}
+        onChange={(e) => {
+          setApiToken(e.currentTarget.value);
+        }}
+      />
+      <button
+        disabled={isStreaming}
+        onClick={() => {
+          setIsStreaming(true);
+          const newChannel = new Misskey.Stream("https://misskey.systems", {
+            token: apiToken,
+          }).useChannel("localTimeline");
+          setChannel(newChannel);
+          newChannel.on("note", (payload) => {
+            console.log(payload);
+          });
+        }}
+      >
+        開始
+      </button>
+      <button
+        disabled={!isStreaming}
+        onClick={() => {
+          setIsStreaming(false);
+          if (!channel) {
+            return;
+          }
+          channel.dispose();
+          setChannel(null);
+        }}
+      >
+        終了
+      </button>
+    </div>
+  );
 };
 
 export default Home;
