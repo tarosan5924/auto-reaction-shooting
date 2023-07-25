b0af001f884df19dc3e531bceab321ddb47b108d
たろ <139237456+tarosan5924@users.noreply.github.com>
2023-07-25 20:47:54 +0900

    レイアウト修正 (#5)
    * タイトルと説明修正
    
    * tailwind使えるように修正
    
    * 入力欄とボタンの位置修正
    
    * ボタンの色修正、タイトル追加
    
    * disabledのボタン文字色変更
    
    * 使い方追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,21 @@ const Home = (): JSX.Element => {
   }>>(null);
 
   return (
-    <div>
+    <div className="container mx-auto my-4 px-4 max-w-xl">
+      <h1 className="text-center text-3xl my-2">
+        コログ構文リアクションシューター
+      </h1>
+      <div className="my-2">
+        使い方
+        <ol className="pl-2 my-1">
+          <li>1.APIトークンを入力</li>
+          <li>2.開始ボタンを押す</li>
+          <li>3.コログ構文のノートが流れてくるのを待つ</li>
+        </ol>
+        以上！！
+      </div>
       <input
+        className="border border-solid border-gray-400 p-1 w-full my-1"
         type="text"
         value={apiToken}
         placeholder={"APIトークンを入力"}
@@ -23,45 +36,49 @@ const Home = (): JSX.Element => {
           setApiToken(e.currentTarget.value);
         }}
       />
-      <button
-        disabled={isStreaming}
-        onClick={() => {
-          setIsStreaming(true);
-          const newChannel = new Misskey.Stream("https://misskey.systems", {
-            token: apiToken,
-          }).useChannel("localTimeline");
-          setChannel(newChannel);
-          newChannel.on("note", async (payload) => {
-            if (
-              payload.text?.includes("てェ…") ||
-              payload.text?.includes("でェ…")
-            ) {
-              await new Misskey.api.APIClient({
-                origin: "https://misskey.systems",
-                credential: apiToken,
-              }).request("notes/reactions/create", {
-                noteId: payload.id,
-                reaction: "🚀",
-              });
+      <div className="flex flex-row m-1">
+        <button
+          className="border m-1 p-1 w-1/2 border-transparent bg-green-400 hover:bg-transparent hover:border-green-400 disabled:bg-gray-200 disabled:hover:border-transparent disabled:text-gray-400"
+          disabled={isStreaming}
+          onClick={() => {
+            setIsStreaming(true);
+            const newChannel = new Misskey.Stream("https://misskey.systems", {
+              token: apiToken,
+            }).useChannel("localTimeline");
+            setChannel(newChannel);
+            newChannel.on("note", async (payload) => {
+              if (
+                payload.text?.includes("てェ…") ||
+                payload.text?.includes("でェ…")
+              ) {
+                await new Misskey.api.APIClient({
+                  origin: "https://misskey.systems",
+                  credential: apiToken,
+                }).request("notes/reactions/create", {
+                  noteId: payload.id,
+                  reaction: "🚀",
+                });
+              }
+            });
+          }}
+        >
+          開始
+        </button>
+        <button
+          className="border m-1 p-1 w-1/2 border-transparent bg-red-400 hover:bg-transparent hover:border-red-400 disabled:bg-gray-200 disabled:hover:border-transparent disabled:text-gray-400"
+          disabled={!isStreaming}
+          onClick={() => {
+            setIsStreaming(false);
+            if (!channel) {
+              return;
             }
-          });
-        }}
-      >
-        開始
-      </button>
-      <button
-        disabled={!isStreaming}
-        onClick={() => {
-          setIsStreaming(false);
-          if (!channel) {
-            return;
-          }
-          channel.dispose();
-          setChannel(null);
-        }}
-      >
-        終了
-      </button>
+            channel.dispose();
+            setChannel(null);
+          }}
+        >
+          終了
+        </button>
+      </div>
     </div>
   );
 };
