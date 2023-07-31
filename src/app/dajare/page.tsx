eac9fb0701263c5f4b72f6213f1ba6ff279eaf9c
たろ <139237456+tarosan5924@users.noreply.github.com>
2023-08-01 00:30:46 +0900

    審議シューティングページ追加 (#11)
    * ダジャレリアクションシューティング用ページ追加
    
    * 審議リアクションシューティング出来るように修正
    
    * 長文は誤検知の可能性が高まるので判定対象とするノートの文字数を制限

---
+++ src/app/dajare/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useState } from "react";
+import * as Misskey from "misskey-js";
+import { dajareWake } from "../service/JudgeDajareService";
+
+const Home = (): JSX.Element => {
+  const [isStreaming, setIsStreaming] = useState(false);
+  const [apiToken, setApiToken] = useState<string>("");
+  const [channel, setChannel] = useState<null | Misskey.ChannelConnection<{
+    params: null;
+    events: {
+      note: (payload: Misskey.entities.Note) => void;
+    };
+    receives: null;
+  }>>(null);
+
+  return (
+    <div className="container mx-auto my-4 px-4 max-w-xl">
+      <h1 className="text-center text-3xl my-2">
+        ダジャレリアクションシューター
+      </h1>
+      <div className="my-2">
+        使い方
+        <ol className="pl-2 my-1">
+          <li>1.APIトークンを入力</li>
+          <li>2.開始ボタンを押す</li>
+          <li>3.ダジャレノートが流れてくるのを待つ</li>
+        </ol>
+        以上！！
+      </div>
+      <input
+        className="border border-solid border-gray-400 p-1 w-full my-1"
+        type="text"
+        value={apiToken}
+        placeholder={"APIトークンを入力"}
+        onChange={(e) => {
+          setApiToken(e.currentTarget.value);
+        }}
+      />
+      <div className="flex flex-row m-1">
+        <button
+          className="border m-1 p-1 w-1/2 border-transparent bg-green-400 hover:bg-transparent hover:border-green-400 disabled:bg-gray-200 disabled:hover:border-transparent disabled:text-gray-400"
+          disabled={isStreaming}
+          onClick={() => {
+            setIsStreaming(true);
+            const newChannel = new Misskey.Stream("https://misskey.systems", {
+              token: apiToken,
+            }).useChannel("localTimeline");
+            setChannel(newChannel);
+            newChannel.on("note", async (payload) => {
+              //長すぎる分は誤検知の可能性が高まるので判定しない
+              if ((payload.text ?? "").length > 15) {
+                return;
+              }
+              if (await dajareWake(payload.text ?? "")) {
+                await new Misskey.api.APIClient({
+                  origin: "https://misskey.systems",
+                  credential: apiToken,
+                }).request("notes/reactions/create", {
+                  noteId: payload.id,
+                  reaction: ":shingi:",
+                });
+              }
+            });
+          }}
+        >
+          開始
+        </button>
+        <button
+          className="border m-1 p-1 w-1/2 border-transparent bg-red-400 hover:bg-transparent hover:border-red-400 disabled:bg-gray-200 disabled:hover:border-transparent disabled:text-gray-400"
+          disabled={!isStreaming}
+          onClick={() => {
+            setIsStreaming(false);
+            if (!channel) {
+              return;
+            }
+            channel.dispose();
+            setChannel(null);
+          }}
+        >
+          終了
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ const Home = (): JSX.Element => {
 
   return (
     <div className="flex flex-col items-center mx-auto my-4 px-4 max-w-xl">
-      <h1 className="text-center text-3xl my-2">リアクションシューター for みすてむず</h1>
+      <h1 className="text-center text-3xl my-2">
+        リアクションシューター for みすてむず
+      </h1>
       <button
         className="border m-1 p-1 w-1/2 hover:bg-transparent border-gray-400 hover:bg-gray-400"
         onClick={() => {
@@ -17,7 +19,9 @@ const Home = (): JSX.Element => {
       </button>
       <button
         className="border m-1 p-1 w-1/2 hover:bg-transparent border-gray-400 hover:bg-gray-400"
-        onClick={() => {}}
+        onClick={() => {
+          router.push("/dajare");
+        }}
       >
         ダジャレ審議
       </button>
