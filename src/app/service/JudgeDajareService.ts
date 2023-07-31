d655e4b426607936e0457e8edb0952917e136903
tarosan5924 <tarosan5924@gmail.com>
2023-07-31 22:42:59 +0900

    constに変える

--- src/app/service/JudgeDajareService.ts
+++ src/app/service/JudgeDajareService.ts
@@ -1,4 +1,8 @@
-import kuromoji, { IpadicFeatures, Tokenizer, TokenizerBuilder } from "kuromoji";
+import kuromoji, {
+  IpadicFeatures,
+  Tokenizer,
+  TokenizerBuilder,
+} from "kuromoji";
 
 const replaceWords: readonly [string, string][] = [
   ["。", ""],
@@ -28,38 +32,39 @@ const replaceWords: readonly [string, string][] = [
   ["ー", ""],
 ];
 
-const defaultBuilder = kuromoji.builder({dicPath:"/dict"})
+const defaultBuilder = kuromoji.builder({ dicPath: "/dict" });
+
 const getTokenizer = (
-    builder: TokenizerBuilder<IpadicFeatures>
-  ): Promise<Tokenizer<IpadicFeatures>> => {
-    return new Promise((resolve, reject) => {
-      builder.build((err, tokenizer) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(tokenizer);
-      });
+  builder: TokenizerBuilder<IpadicFeatures>
+): Promise<Tokenizer<IpadicFeatures>> => {
+  return new Promise((resolve, reject) => {
+    builder.build((err, tokenizer) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(tokenizer);
     });
+  });
 };
 
-function preprocessing(sentence: string): string {
+const preprocessing = (sentence: string): string => {
   for (const [from, to] of replaceWords) {
     sentence = sentence.replace(new RegExp(from, "g"), to);
   }
 
   return sentence;
-}
+};
 
-function divide(sentence: string, n: number): string[] {
+const divide = (sentence: string, n: number): string[] => {
   const elements: string[] = [];
   const repeatNum = sentence.length - (n - 1);
   for (let i = 0; i < repeatNum; i++) {
     elements.push(sentence.slice(i, i + n));
   }
   return elements;
-}
+};
 
-function listMaxDup(divided: string[]): [string, number] {
+const listMaxDup = (divided: string[]): [string, number] => {
   const counts = new Map<string, number>();
   for (const element of divided) {
     counts.set(element, (counts.get(element) || 0) + 1);
@@ -73,20 +78,20 @@ function listMaxDup(divided: string[]): [string, number] {
     }
   }
   return [maxElement, maxCount];
-}
+};
 
-function sentenceMaxDupRate(divided: string[], target: string): number {
+const sentenceMaxDupRate = (divided: string[], target: string): number => {
   return (
     divided.filter((element) => element === target).length / divided.length
   );
-}
+};
 
-function dajareWake(sentence: string, n: number = 3): boolean {
+export const dajareWake = (sentence: string, n: number = 3): boolean => {
   // MeCabの代わりに直接処理したものを使用
   // TODO kuromojiを使って置き換える
   const preprocessed = preprocessing(sentence);
   const divided = divide(preprocessed, n);
-  kuromoji.builder({dicPath:"/dict"})
+  kuromoji.builder({ dicPath: "/dict" });
   if (divided.length === 0) {
     return false;
   } else {
@@ -97,4 +102,4 @@ function dajareWake(sentence: string, n: number = 3): boolean {
       return false;
     }
   }
-}
+};
